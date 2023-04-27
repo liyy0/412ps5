83799ef0cc3abb52d26167d728b9251c69c719d7
liyy0 <yuyanli@bu.edu>
2023-04-27 18:37:39 -0400

    almost finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { weatherData } from 'src/data';
+
+interface Weather{
+  city: string;
+  temperature: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = '412ps5';
+  messages:Weather[] = [];
+  onClick(){
+    console.log("clicked");
+    this.messages = weatherData;
+  }
 }
+JSON.stringify(weatherData);
---
+++ src/data.ts
@@ -0,0 +1,28 @@
+export const weatherData = [
+    {
+      "city": "New York",
+      "temperature": 14,
+      "description": "Sunny"
+    },
+    {
+      "city": "Los Angeles",
+      "temperature": 24,
+      "description": "Cloudy"
+    },
+    {
+      "city": "Chicago",
+      "temperature": 8,
+      "description": "Rainy"
+    },
+    {
+      "city": "Houston",
+      "temperature": 18,
+      "description": "Partly cloudy"
+    },
+    {
+      "city": "Miami",
+      "temperature": 26,
+      "description": "Thunderstorm"
+    }
+  ];
+  
